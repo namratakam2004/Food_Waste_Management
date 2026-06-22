56ce1574d4d62595c7b350ebb21eaa89043188d9
Namrata Kamble <namratakam2004@gmail.com>
2026-06-22 21:53:38 +0530

    added self explanatory video

--- database/db_connection.py
+++ database/db_connection.py
@@ -2,10 +2,10 @@ import psycopg2
 
 def get_connection():
     conn = psycopg2.connect(
-        host="ep-shiny-frog-atr05zw4-pooler.c-9.us-east-1.aws.neon.tech",
+        host="DATABASE URL",
         database="neondb",
         user="neondb_owner",
-        password="npg_5GAOUeQaZp4W",
+        password="*********",
         sslmode="require"
     )
     return conn
